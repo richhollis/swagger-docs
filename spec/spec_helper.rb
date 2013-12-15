6a4afdaf81f3582d0af1e272967f0e0b47b6c0eb
Darius Roberts <darius.roberts@gmail.com>
2013-12-15 10:20:58 -0500

    use more readable 'exist' rspec matcher

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,12 @@ require "rails"
 require "swagger/docs"
 require "ostruct"
 require "json"
+require 'pathname'
 
-TMP_DIR = "/tmp/swagger-docs/"
-TMP_API_DIR = "/tmp/swagger-docs/api/v1/"
-FILE_RESOURCES = "#{TMP_API_DIR}api-docs.json"
-FILE_RESOURCE = "#{TMP_API_DIR}sample.json"
+TMP_DIR = Pathname.new "/tmp/swagger-docs/"
+TMP_API_DIR = TMP_DIR+"api/v1"
+FILE_RESOURCES = TMP_API_DIR+"api-docs.json"
+FILE_RESOURCE = TMP_API_DIR+"sample.json"
 
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
