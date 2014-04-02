09a6d772e6653ee13cbd404cbba73ca2a6444271
Rich Hollis <richhollis@gmail.com>
2014-04-02 16:33:21 +0100

    Improve structure of generator class - break up large methods

--- lib/swagger/docs/generator.rb
+++ lib/swagger/docs/generator.rb
@@ -12,42 +12,6 @@ module Swagger
 
       class << self
 
-        def camelize_keys_deep!(h)
-          h.keys.each do |k|
-            ks    = k.to_s.camelize(:lower)
-            h[ks] = h.delete k
-            camelize_keys_deep! h[ks] if h[ks].kind_of? Hash
-            if h[ks].kind_of? Array
-              h[ks].each do |a|
-                next unless a.kind_of? Hash
-                camelize_keys_deep! a
-              end
-            end
-          end
-        end
-
-        def transform_spec_to_api_path(spec, controller_base_path, extension)
-           api_path = spec.to_s.dup
-           api_path.gsub!('(.:format)', extension ? ".#{extension}" : '')
-           api_path.gsub!(/:(\w+)/, '{\1}')
-           api_path.gsub!(controller_base_path, '')
-           trim_slashes(api_path)
-          end
-
-        def trim_leading_slash(str)
-          return str if !str
-          str.gsub(/\A\/+/, '')
-        end
-
-        def trim_trailing_slash(str)
-          return str if !str
-          str.gsub(/\/+\z/, '')
-        end
-
-        def trim_slashes(str)
-          trim_leading_slash(trim_trailing_slash(str))
-        end
-
         def set_real_methods
           Config.base_api_controller.send(:include, Methods) # replace impotent methods with live ones
         end
@@ -68,76 +32,158 @@ module Swagger
         end
 
         def write_doc(api_version, config)
-          base_path = trim_trailing_slash(config[:base_path] || "")
-          controller_base_path = trim_leading_slash(config[:controller_base_path] || "")
-          api_file_path = config[:api_file_path]
-          clean_directory = config[:clean_directory] || false
+          settings = get_settings(api_version, config)
+
+          create_output_paths(settings[:api_file_path])
+          clean_output_paths(settings[:api_file_path]) if config[:clean_directory] || false
+
+          root = { :api_version => api_version, :swagger_version => "1.2", :base_path => settings[:base_path] + "/", :apis => []}
           results = {:processed => [], :skipped => []}
 
-          # create output paths
-          FileUtils.mkdir_p(api_file_path) # recursively create out output path
-          Dir.foreach(api_file_path) {|f| fn = File.join(api_file_path, f); File.delete(fn) if !File.directory?(fn) and File.extname(fn) == '.json'} if clean_directory # clean output path
-
-          base_path += "/#{controller_base_path}" unless controller_base_path.empty?
-          root = { :api_version => api_version, :swagger_version => "1.2", :base_path => base_path + "/"}
-          resources = root.merge({:apis => []})
-
-          paths = Config.base_application.routes.routes.map{|i| "#{i.defaults[:controller]}" }
-          paths = paths.uniq.select{|i| i.start_with?(controller_base_path)}
-          paths.each do |path|
-            next if path.empty?
-            klass = "#{path.to_s.camelize}Controller".constantize
-            if !klass.methods.include?(:swagger_config) or !klass.swagger_config[:controller]
-              results[:skipped] << path
-              next
+          get_route_paths(settings[:controller_base_path]).each do |path|
+            ret = process_path(path, root, config, settings)
+            results[ret[:action]] << ret
+            if ret[:action] == :processed
+              create_resource_file(ret[:path], ret[:apis], ret[:models], settings, root, config)
+              debased_path = get_debased_path(ret[:path], settings[:controller_base_path])
+              resource_api = {
+                path: "#{Config.transform_path(trim_leading_slash(debased_path))}.{format}",
+                description: ret[:klass].swagger_config[:description]
+              }
+              root[:apis] << resource_api
             end
-            apis = []
-            models = {}
-            debased_path = path.gsub("#{controller_base_path}", "")
-            Config.base_application.routes.routes.select{|i| i.defaults[:controller] == path}.each do |route|
-              action = route.defaults[:action]
-              verb = route.verb.source.to_s.delete('$'+'^').downcase.to_sym
-              next if !operations = klass.swagger_actions[action.to_sym]
-              operations = Hash[operations.map {|k, v| [k.to_s.gsub("@","").to_sym, v.respond_to?(:deep_dup) ? v.deep_dup : v.dup] }] # rename :@instance hash keys
-              operations[:method] = verb
-              operations[:nickname] = "#{path.camelize}##{action}"
-              
-              api_path = transform_spec_to_api_path(route.path.spec, controller_base_path, config[:api_extension_type])
-              operations[:parameters] = filter_path_params(api_path, operations[:parameters]) if operations[:parameters]
+          end
 
-              apis << {:path => api_path, :operations => [operations]}
+          camelize_keys_deep!(root)
+          write_to_file("#{settings[:api_file_path]}/api-docs.json", root, config)
+          results
+        end
 
-              # Add any declared models to the root of the resource.
-              klass.swagger_models.each do |model_name, model|
-                formatted_model = {
-                  id: model[:id],
-                  required: model[:required],
-                  properties: model[:properties],
-                }
-                formatted_model[:description] = model[:description] if model[:description]
-                models[model[:id]] = formatted_model
+        private
+
+        def transform_spec_to_api_path(spec, controller_base_path, extension)
+          api_path = spec.to_s.dup
+          api_path.gsub!('(.:format)', extension ? ".#{extension}" : '')
+          api_path.gsub!(/:(\w+)/, '{\1}')
+          api_path.gsub!(controller_base_path, '')
+          trim_slashes(api_path)
+        end
+
+        def camelize_keys_deep!(h)
+          h.keys.each do |k|
+            ks    = k.to_s.camelize(:lower)
+            h[ks] = h.delete k
+            camelize_keys_deep! h[ks] if h[ks].kind_of? Hash
+            if h[ks].kind_of? Array
+              h[ks].each do |a|
+                next unless a.kind_of? Hash
+                camelize_keys_deep! a
               end
             end
-            demod = "#{debased_path.to_s.camelize}".demodulize.camelize.underscore
-            resource = root.merge({:resource_path => "#{demod}", :apis => apis})
-            camelize_keys_deep!(resource)
-
-            # Add the already-normalized models to the resource.
-            resource = resource.merge({:models => models}) if models.present?
-            # write controller resource file
-            write_to_file "#{api_file_path}/#{demod}.json", resource, config
-            # append resource to resources array (for writing out at end)
-            resources[:apis] << {
-              path: "#{Config.transform_path(trim_leading_slash(debased_path))}.{format}",
-              description: klass.swagger_config[:description]
-            }
-            results[:processed] << path
           end
-          # write master resource file
-          camelize_keys_deep!(resources)
+        end
 
-          write_to_file "#{api_file_path}/api-docs.json", resources, config
-          results
+        def trim_leading_slash(str)
+          return str if !str
+          str.gsub(/\A\/+/, '')
+        end
+
+        def trim_trailing_slash(str)
+          return str if !str
+          str.gsub(/\/+\z/, '')
+        end
+
+        def trim_slashes(str)
+          trim_leading_slash(trim_trailing_slash(str))
+        end
+
+        def get_debased_path(path, controller_base_path)
+          path.gsub("#{controller_base_path}", "")
+        end
+
+        def process_path(path, root, config, settings)
+          return {action: empty} if path.empty?
+          klass = "#{path.to_s.camelize}Controller".constantize
+          return {action: :skipped, path: path} if !klass.methods.include?(:swagger_config) or !klass.swagger_config[:controller]
+          apis, models = [], {}
+          Config.base_application.routes.routes.select{|i| i.defaults[:controller] == path}.each do |route|
+            ret = get_route_path_apis(path, route, klass, settings, config)
+            apis = apis + ret[:apis]
+            models.merge!(ret[:models])
+          end
+          {action: :processed, path: path, apis: apis, models: models, klass: klass}
+        end
+
+        def create_resource_file(path, apis, models, settings, root, config)
+          debased_path = get_debased_path(path, settings[:controller_base_path])
+          demod = "#{debased_path.to_s.camelize}".demodulize.camelize.underscore
+          resource = root.merge({:resource_path => "#{demod}", :apis => apis})
+          camelize_keys_deep!(resource)
+          # Add the already-normalized models to the resource.
+          resource = resource.merge({:models => models}) if models.present?
+          # write controller resource file
+          write_to_file "#{settings[:api_file_path]}/#{demod}.json", resource, config
+        end
+
+        def get_route_path_apis(path, route, klass, settings, config)
+          models, apis = {}, []
+          action = route.defaults[:action]
+          verb = route.verb.source.to_s.delete('$'+'^').downcase.to_sym
+          return apis if !operations = klass.swagger_actions[action.to_sym]
+          operations = Hash[operations.map {|k, v| [k.to_s.gsub("@","").to_sym, v.respond_to?(:deep_dup) ? v.deep_dup : v.dup] }] # rename :@instance hash keys
+          operations[:method] = verb
+          operations[:nickname] = "#{path.camelize}##{action}"
+          
+          api_path = transform_spec_to_api_path(route.path.spec, settings[:controller_base_path], config[:api_extension_type])
+          operations[:parameters] = filter_path_params(api_path, operations[:parameters]) if operations[:parameters]
+
+          apis << {:path => api_path, :operations => [operations]}
+          models = get_klass_models(klass)          
+
+          {apis: apis, models: models}
+        end
+
+        def get_klass_models(klass)
+          models = {}
+          # Add any declared models to the root of the resource.
+          klass.swagger_models.each do |model_name, model|
+            formatted_model = {
+              id: model[:id],
+              required: model[:required],
+              properties: model[:properties],
+            }
+            formatted_model[:description] = model[:description] if model[:description]
+            models[model[:id]] = formatted_model
+          end
+          models
+        end
+
+        def get_settings(api_version, config)
+          base_path = trim_trailing_slash(config[:base_path] || "")
+          controller_base_path = trim_leading_slash(config[:controller_base_path] || "")
+          base_path += "/#{controller_base_path}" unless controller_base_path.empty?
+          api_file_path = config[:api_file_path]          
+          settings = { 
+            base_path: base_path,
+            controller_base_path: controller_base_path,
+            api_file_path: api_file_path
+          }.freeze
+        end
+
+        def get_route_paths(controller_base_path)
+          paths = Config.base_application.routes.routes.map{|i| "#{i.defaults[:controller]}" }
+          paths.uniq.select{|i| i.start_with?(controller_base_path)}
+        end
+
+        def create_output_paths(api_file_path)
+          FileUtils.mkdir_p(api_file_path) # recursively create out output path
+        end
+
+        def clean_output_paths(api_file_path)
+          Dir.foreach(api_file_path) do |f|
+            fn = File.join(api_file_path, f)
+            File.delete(fn) if !File.directory?(fn) and File.extname(fn) == '.json'
+          end
         end
 
         def write_to_file(path, structure, config={})
@@ -147,8 +193,6 @@ module Swagger
           end
           File.open(path, 'w') { |file| file.write content }
         end
-
-        private
 
         def filter_path_params(path, params)
           params.reject do |param|
